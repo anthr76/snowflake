07f50eabb33b35076091a9955aa32df2a1a89980
Anthony Rabbito <hello@anthonyrabbito.com>
2022-04-15 11:28:29 -0400

    the g14 is getting returned but the code isn't
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- profiles/graphical/audio/default.nix
+++ profiles/graphical/audio/default.nix
@@ -10,5 +10,6 @@
   environment.systemPackages = with pkgs; [
     pavucontrol
     pulseaudio  # pactl
+    easyeffects
   ];
 }
--- users/anthonyjrabbito/default.nix
+++ users/anthonyjrabbito/default.nix
@@ -1,7 +1,6 @@
 { hmUsers, pkgs, config, ... }:
 {
   home-manager.users = { inherit (hmUsers) anthonyjrabbito; };
-
   users.defaultUserShell = pkgs.fish;
   users.users.anthonyjrabbito = {
     description = "default";
