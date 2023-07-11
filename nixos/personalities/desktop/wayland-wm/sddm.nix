02da535009c594a05058717bb170a4f483ec4518
Anthony Rabbito <hello@anthonyrabbito.com>
2023-07-11 03:19:21 +0000

    hack: enable silly X
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- nixos/personalities/desktop/wayland-wm/sddm.nix
+++ nixos/personalities/desktop/wayland-wm/sddm.nix
@@ -1,5 +1,7 @@
 { pkgs, ... }:
 {
+  # TODO: In 2023 SDDM should not need X11 to start. Make that go away.
+  services.xserver.enable = true;
   services.xserver.displayManager.sddm = {
     enable = true;
     settings = {
