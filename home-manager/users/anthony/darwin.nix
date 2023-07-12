a07bcfea7addfd091a36b3bf7d10173251738506
Anthony Rabbito <hello@anthonyrabbito.com>
2023-07-12 22:57:04 +0000

    fix: yubi darwin
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- home-manager/users/anthony/darwin.nix
+++ home-manager/users/anthony/darwin.nix
@@ -3,8 +3,12 @@
     username = "anthony";
     homeDirectory = "/Users/anthony";
   };
-
+  # Yubikey glue usually done only in NixOS.
   home.packages = with pkgs; [
     yubico-piv-tool
   ];
+  programs.ssh.extraConfig = ''
+    Host *
+      PKCS11Provider "${pkgs.yubico-piv-tool}/lib/libykcs11.so"
+  '';
 }
