3a481adff9f6460d685a814a03c241e3841ea667
Anthony Rabbito <hello@anthonyrabbito.com>
2023-07-21 21:32:25 -0400

    fix: e39 testing
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- nixos/hosts/e39.nwk3.rabbito.tech/default.nix
+++ nixos/hosts/e39.nwk3.rabbito.tech/default.nix
@@ -18,52 +18,6 @@
     luksCreds = config.sops.secrets.e39-luks-password.path;
   };
 
-  boot.initrd.luks.devices.root = lib.mkForce
-    {
-      device = "/dev/disk/by-partlabel/crypted";
-    };
-
-  fileSystems."/" = lib.mkForce
-    { device = "/dev/mapper/root";
-      fsType = "btrfs";
-      options = [ "subvol=rootfs" ];
-    };
-
-  fileSystems."/srv" = lib.mkForce
-    { device = "/dev/mapper/root";
-      fsType = "btrfs";
-      options = [ "subvol=rootfs/srv" ];
-    };
-
-  fileSystems."/var/lib/portables" = lib.mkForce
-    { device = "/dev/mapper/root";
-      fsType = "btrfs";
-      options = [ "subvol=rootfs/var/lib/portables" ];
-    };
-
-  fileSystems."/var/lib/machines" = lib.mkForce
-    { device = "/dev/mapper/root";
-      fsType = "btrfs";
-      options = [ "subvol=rootfs/var/lib/machines" ];
-    };
-
-  fileSystems."/boot" = lib.mkForce
-    { device = "/dev/disk/by-partlabel/ESP";
-      fsType = "vfat";
-    };
-
-  fileSystems."/nix" = lib.mkForce
-    { device = "/dev/mapper/root";
-      fsType = "btrfs";
-      options = [ "subvol=nix" ];
-    };
-
-  fileSystems."/home" = lib.mkForce
-    { device = "/dev/mapper/root";
-      fsType = "btrfs";
-      options = [ "subvol=home" ];
-    };
-
 
   swapDevices = [ ];
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
