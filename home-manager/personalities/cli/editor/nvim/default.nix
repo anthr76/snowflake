fcdcfb49d3f84433f06e9e413f320ea511cc6d0b
Anthony Rabbito <hello@anthonyrabbito.com>
2023-07-26 12:52:05 +0000

    fix: add wip
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- home-manager/personalities/cli/editor/nvim/default.nix
+++ home-manager/personalities/cli/editor/nvim/default.nix
@@ -1,10 +1,21 @@
 { config, pkgs, ... }:
+let
+  astroNvimSource = pkgs.fetchFromGitHub {
+    owner = "AstroNvim";
+    repo = "AstroNvim";
+    rev = "96ddbcc391b6ef5fa58c9d6ce9a8334e86b65f20";
+    sha256 = "LVaeoR34nwQEZXpNryPLh4RRVQvWuyq3tAGaW3iYHow=";
+  };
+in
 {
   programs.neovim = {
     enable = true;
     defaultEditor = true;
     vimAlias = true;
     viAlias = true;
+    extraLuaConfig = ''
+    vim.opt.rtp:append("${config.xdg.configHome}/nvim/lua/user")
+    '';
     # https://astronvim.com/#-requirements
     extraPackages = [
       pkgs.nerdfonts
@@ -21,20 +32,17 @@
   };
   xdg.configFile = {
     # Not Very Idempotent yet :(
-    astrovim = {
+    userConfig = {
       onChange = "${pkgs.neovim}/bin/nvim --headless +quitall";
-      target = "nvim/astrovim";
-      source = pkgs.fetchFromGitHub {
-        owner = "AstroNvim";
-        repo = "AstroNvim";
-        rev = "96ddbcc391b6ef5fa58c9d6ce9a8334e86b65f20";
-        sha256 = "LVaeoR34nwQEZXpNryPLh4RRVQvWuyq3tAGaW3iYHow=";
-      };
-    };
-    lua = {
-      source = ./lua;
       recursive = true;
       target = "nvim/lua/user";
+      source = ./lua;
+    };
+    astrovim = {
+      onChange = "${pkgs.neovim}/bin/nvim --headless +quitall";
+      recursive = true;
+      target = "nvim/astrovim";
+      source = astroNvimSource;
     };
   };
   home.sessionVariables = {
