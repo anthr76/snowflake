af935af879b1fa66a9ab41e7a6ab848ffab81509
Anthony Rabbito <hello@anthonyrabbito.com>
2023-07-31 19:50:23 -0400

    fix: hypr test
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- home-manager/personalities/desktop/wayland-wm/hyprland/default.nix
+++ home-manager/personalities/desktop/wayland-wm/hyprland/default.nix
@@ -2,7 +2,7 @@
   imports = [
     inputs.hyprland.homeManagerModules.default
   ];
-  inputs.wayland.windowManager.hyprland = {
+  wayland.windowManager.hyprland = {
     settings = {
       general = {
         gaps_in = 15;
