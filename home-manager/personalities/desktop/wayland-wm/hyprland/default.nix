21f8b770da8f001165043b1f2444e9535738b7ad
Anthony Rabbito <hello@anthonyrabbito.com>
2023-07-31 19:20:55 -0400

    fix: hypr test
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- home-manager/personalities/desktop/wayland-wm/hyprland/default.nix
+++ home-manager/personalities/desktop/wayland-wm/hyprland/default.nix
@@ -1,8 +1,21 @@
 {
-  xdg.configFile = {
-    hyprConfig = {
-      target = "hypr/hyperland.conf";
-      source = "./hyprland.conf";
+   wayland.windowManager.hyprland = {
+    settings = {
+      general = {
+        gaps_in = 15;
+        gaps_out = 20;
+        border_size = 2.7;
+      };
+      input = {
+        kb_layout = "en,us";
+        touchpad.disable_while_typing = true;
+      };
+      dwindle.split_width_multiplier = 1.35;
+
+      animations = {
+        enabled = true;
+      };
+
     };
   };
 }
