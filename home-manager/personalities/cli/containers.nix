826e94e6e863525c52bdb40a347f1f245c7852bd
Anthony Rabbito <hello@anthonyrabbito.com>
2023-08-10 15:40:32 -0400

    fix: make docker only shortfile

--- home-manager/personalities/cli/containers.nix
+++ home-manager/personalities/cli/containers.nix
@@ -4,7 +4,7 @@
       target = "containers/registries.conf.d/001-home-manager.conf";
       text = ''
         # Managed with Home Manager
-        unqualified-search-registries = ["registry.fedoraproject.org", "registry.access.redhat.com", "quay.io", "registry.redhat.io", "docker.io"]
+        unqualified-search-registries = ["docker.io"]
       '';
     };
   };
