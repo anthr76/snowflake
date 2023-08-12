8573007e40cc2c690e722b8ed17d0eab41efbb6d
Anthony Rabbito <hello@anthonyrabbito.com>
2023-08-12 14:20:47 -0400

    fix

--- nixos/personalities/base/bootloader.nix
+++ nixos/personalities/base/bootloader.nix
@@ -3,9 +3,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.systemd.enable = true;
   boot.initrd.systemd.emergencyAccess = true;
-  boot.loader.systemd-boot.consoleMode = "auto";
+    boot.initrd.supportedFilesystems = [ "btrfs" ];
   boot.supportedFilesystems = [ "btrfs" ];
   boot.initrd.enable = true;
-
-  boot.initrd.supportedFilesystems = [ "btrfs" ];
 }
