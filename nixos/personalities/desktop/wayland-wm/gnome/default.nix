cd59c3a93eb1d1222bfcac5905c667597b84b325
Anthony Rabbito <hello@anthonyrabbito.com>
2023-08-12 15:52:12 -0400

    enable

--- nixos/personalities/desktop/wayland-wm/gnome/default.nix
+++ nixos/personalities/desktop/wayland-wm/gnome/default.nix
@@ -4,11 +4,13 @@
   ];
   services = {
     xserver = {
+      enable = true;
       desktopManager.gnome = {
         enable = true;
       };
       displayManager.gdm = {
         enable = true;
+        wayland = true;
         autoSuspend = true;
       };
     };
