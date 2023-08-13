516fe62e1e50d5b986e0c37a3afff5f66f494f54
Anthony Rabbito <hello@anthonyrabbito.com>
2023-08-13 09:35:26 -0400

    gnome todo

--- home-manager/hosts/e39.nwk3.rabbito.tech.nix
+++ home-manager/hosts/e39.nwk3.rabbito.tech.nix
@@ -3,7 +3,7 @@
     ../users/anthony
     ../users/anthony/linux.nix
     ../personalities/desktop
-    ../personalities/desktop/wayland-wm/hyprland
+    # ../personalities/desktop/wayland-wm/hyprland
   ];
 
 }
