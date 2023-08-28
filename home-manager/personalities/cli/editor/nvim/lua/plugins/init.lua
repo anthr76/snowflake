35f0e5153171b8fa3170cd656284fb68ced759d2
Anthony Rabbito <hello@anthonyrabbito.com>
2023-08-28 19:27:21 -0400

    fix: neovim
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- home-manager/personalities/cli/editor/nvim/lua/plugins/init.lua
+++ home-manager/personalities/cli/editor/nvim/lua/plugins/init.lua
@@ -1,6 +1,16 @@
 return {
   {
     "towolf/vim-helm",
-    lazy = false
+    event = "VeryLazy",
   },
+  {
+    "folke/todo-comments.nvim",
+    event = "BufReadPost",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  }
 }
