120fb9ba8e52a5aa166e7266abfb27f157491820
Anthony Rabbito <hello@anthonyrabbito.com>
2023-08-30 22:24:28 -0400

    feat: add kubevirt
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- home-manager/personalities/cli/kubernetes.nix
+++ home-manager/personalities/cli/kubernetes.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [ kubectl kubecolor kubelogin-oidc kubernetes-helm stern fluxcd ];
+  home.packages = with pkgs; [ kubectl kubecolor kubelogin-oidc kubernetes-helm stern fluxcd kubevirt ];
   programs.fish.functions = {
     k = {
       wraps = "kubectl";
