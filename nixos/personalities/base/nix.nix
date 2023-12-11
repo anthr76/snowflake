bf4155488704e4fc539aaba30cf84a9be5f06cb9
Anthony Rabbito <hello@anthonyrabbito.com>
2023-12-10 23:11:58 -0500

    fix: nix flake registry
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- nixos/personalities/base/nix.nix
+++ nixos/personalities/base/nix.nix
@@ -1,6 +1,16 @@
 { inputs, lib, ... }:
 {
   nix = {
+    # This will additionally add your inputs to the system's legacy channels
+    # Making legacy nix commands consistent as well, awesome!
+    nixPath = [ "nixpkgs=${inputs.nixpkgs.outPath}" ];
+    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      # Keep the last 3 generations
+      options = "--delete-older-than +3";
+    };
     settings = {
       substituters = ["https://hyprland.cachix.org"];
       trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
@@ -8,6 +18,7 @@
       auto-optimise-store = lib.mkDefault true;
       experimental-features = [ "nix-command" "flakes" "repl-flake" ];
       warn-dirty = false;
+      flake-registry = ""; # Disable global flake registry
     };
   };
 }
