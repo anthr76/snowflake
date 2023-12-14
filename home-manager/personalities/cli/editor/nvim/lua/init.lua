c2eb116750f28e1d7566fb68b7e7824264ea4883
Anthony Rabbito <hello@anthonyrabbito.com>
2023-12-14 14:48:10 -0500

    cleanup

--- home-manager/personalities/cli/editor/nvim/lua/init.lua
+++
@@ -1,7 +0,0 @@
-return {
-  vim.cmd([[
-    autocmd BufRead,BufNewFile */templates/*.yml,*/templates/*.tpl,*.gotmpl,helmfile*.yml set ft=helm
-    autocmd BufRead,BufNewFile */templates/*.yml,*/templates/*.tpl,*.gotmpl,helmfile*.yml LspStop yammls
-  ]])
- 
-}
