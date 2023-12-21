1c0604a32505d6608daff209d1ea59f1a3119537
Anthony Rabbito <hello@anthonyrabbito.com>
2023-12-21 17:24:17 -0500

    helm lua

--- home-manager/personalities/cli/editor/nvim/default.nix
+++ home-manager/personalities/cli/editor/nvim/default.nix
@@ -14,36 +14,13 @@
       recursive = false;
       target = "lvim/config.lua";
       text = /* lua */ ''
-        local configs = require('lspconfig.configs')
-        local lspconfig = require('lspconfig')
-        local util = require('lspconfig.util')
-
-        if not configs.helm_ls then
-          configs.helm_ls = {
-            default_config = {
-              cmd = {"helm_ls", "serve"},
-              filetypes = {'helm'},
-              root_dir = function(fname)
-                return util.root_pattern('Chart.yaml')(fname)
-              end,
-            },
-          }
-        end
-
-        lspconfig.helm_ls.setup {
-          filetypes = {"helm"},
-          cmd = {"helm_ls", "serve"},
-        vim.cmd([[
-          autocmd BufRead,BufNewFile */templates/*.y*ml,*/templates/*.tpl,*.gotmpl,helmfile*.yml set ft=helm
-          autocmd BufRead,BufNewFile */templates/*.y*ml,*/templates/*.tpl,*.gotmpl,helmfile*.yml LspStop yammls
-        ]])
         lvim.plugins = {
           {
             "towolf/vim-helm",
             event = "VeryLazy",
           },
           {
-            "folke/todo-comments.nvim,"
+            "folke/todo-comments.nvim",
             event = "BufReadPost",
             dependencies = { "nvim-lua/plenary.nvim" },
             opts = {
@@ -67,6 +44,44 @@
             end
           },
         }
+      '';
+    };
+    helm = {
+      recursive = false;
+      target = "lvim/helm.lua";
+      text = /* lua */ ''
+        vim.cmd([[
+          autocmd BufRead,BufNewFile */templates/*.yml,*/templates/*.tpl,*.gotmpl,helmfile*.yml set ft=helm
+          autocmd BufRead,BufNewFile */templates/*.yml,*/templates/*.tpl,*.gotmpl,helmfile*.yml LspStop yammls
+        ]])
+        local configs = require('lspconfig.configs')
+        local lspconfig = require('lspconfig')
+        local util = require('lspconfig.util')
+
+        if not configs.helm_ls then
+          configs.helm_ls = {
+            default_config = {
+              cmd = {"helm_ls", "serve"},
+              filetypes = {'helm'},
+              root_dir = function(fname)
+                return util.root_pattern('Chart.yaml')(fname)
+              end,
+            },
+          }
+        end
+        lspconfig.helm_ls.setup {
+          filetypes = {"helm"},
+          cmd = {"helm_ls", "serve"},
+        }
+        -- Disable YAMLLS for Helm chart files
+        vim.cmd([[
+          autocmd FileType yaml if getline(1) =~# '^#.*helm' | LspStop | endif
+        ]])
+        -- Configure LSP with your custom on_attach
+        lspconfig.your_lsp_server.setup({
+            on_attach = custom_on_attach,
+            -- Other configuration options
+        })
       '';
     };
   };
