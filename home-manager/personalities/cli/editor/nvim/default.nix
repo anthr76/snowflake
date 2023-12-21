aee4c252901479c98ce7d8e15f51e0b1e37495d7
Anthony Rabbito <hello@anthonyrabbito.com>
2023-12-21 17:05:54 -0500

    fix helm

--- home-manager/personalities/cli/editor/nvim/default.nix
+++ home-manager/personalities/cli/editor/nvim/default.nix
@@ -14,9 +14,26 @@
       recursive = false;
       target = "lvim/config.lua";
       text = /* lua */ ''
+
+          if not configs.helm_ls then
+            configs.helm_ls = {
+              default_config = {
+                cmd = {"helm_ls", "serve"},
+                filetypes = {'helm'},
+                root_dir = function(fname)
+                  return util.root_pattern('Chart.yaml')(fname)
+                end,
+              },
+            }
+          end
+
+          lspconfig.helm_ls.setup {
+            filetypes = {"helm"},
+            cmd = {"helm_ls", "serve"},
+          }
         vim.cmd([[
-          autocmd BufRead,BufNewFile */templates/*.yml,*/templates/*.tpl,*.gotmpl,helmfile*.yml set ft=helm
-          autocmd BufRead,BufNewFile */templates/*.yml,*/templates/*.tpl,*.gotmpl,helmfile*.yml LspStop yammls
+          autocmd BufRead,BufNewFile */templates/*.y*ml,*/templates/*.tpl,*.gotmpl,helmfile*.yml set ft=helm
+          autocmd BufRead,BufNewFile */templates/*.y*ml,*/templates/*.tpl,*.gotmpl,helmfile*.yml LspStop yammls
         ]])
         lvim.plugins = {
           {
@@ -24,7 +41,7 @@
             event = "VeryLazy",
           },
           {
-            "folke/todo-comments.nvim",
+            "folke/todo-comments.nvim,"
             event = "BufReadPost",
             dependencies = { "nvim-lua/plenary.nvim" },
             opts = {
