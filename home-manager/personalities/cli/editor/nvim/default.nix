520aec372132c9749ec58946ed8cdf3505127155
Anthony Rabbito <hello@anthonyrabbito.com>
2023-12-21 17:14:54 -0500

    fix: helm ls

--- home-manager/personalities/cli/editor/nvim/default.nix
+++ home-manager/personalities/cli/editor/nvim/default.nix
@@ -14,23 +14,25 @@
       recursive = false;
       target = "lvim/config.lua";
       text = /* lua */ ''
+        local configs = require('lspconfig.configs')
+        local lspconfig = require('lspconfig')
+        local util = require('lspconfig.util')
 
-          if not configs.helm_ls then
-            configs.helm_ls = {
-              default_config = {
-                cmd = {"helm_ls", "serve"},
-                filetypes = {'helm'},
-                root_dir = function(fname)
-                  return util.root_pattern('Chart.yaml')(fname)
-                end,
-              },
-            }
-          end
-
-          lspconfig.helm_ls.setup {
-            filetypes = {"helm"},
-            cmd = {"helm_ls", "serve"},
+        if not configs.helm_ls then
+          configs.helm_ls = {
+            default_config = {
+              cmd = {"helm_ls", "serve"},
+              filetypes = {'helm'},
+              root_dir = function(fname)
+                return util.root_pattern('Chart.yaml')(fname)
+              end,
+            },
           }
+        end
+
+        lspconfig.helm_ls.setup {
+          filetypes = {"helm"},
+          cmd = {"helm_ls", "serve"},
         vim.cmd([[
           autocmd BufRead,BufNewFile */templates/*.y*ml,*/templates/*.tpl,*.gotmpl,helmfile*.yml set ft=helm
           autocmd BufRead,BufNewFile */templates/*.y*ml,*/templates/*.tpl,*.gotmpl,helmfile*.yml LspStop yammls
