1f87d008609bb25c192f629873eddaafaadeaa5e
Anthony Rabbito <hello@anthonyrabbito.com>
2023-12-22 14:11:38 -0500

    fix: copiloit
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- home-manager/personalities/cli/editor/nvim/default.nix
+++ home-manager/personalities/cli/editor/nvim/default.nix
@@ -47,6 +47,17 @@
               require("copilot").setup({})
             end
           },
+          {
+            "zbirenbaum/copilot-cmp",
+              event = "InsertEnter",
+              dependencies = { "zbirenbaum/copilot.lua" },
+              config = function()
+                vim.defer_fn(function()
+                  require("copilot").setup() -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+                  require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+                end, 100)
+              end
+          },
         }
       '';
     };
