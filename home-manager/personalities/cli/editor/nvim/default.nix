8ccb7c5d387d676330cb8d9723203cc28d2c342a
Anthony Rabbito <hello@anthonyrabbito.com>
2024-02-13 11:29:51 -0500

    testing

--- home-manager/personalities/cli/editor/nvim/default.nix
+++ home-manager/personalities/cli/editor/nvim/default.nix
@@ -17,7 +17,8 @@
         vim.cmd([[
           autocmd BufRead,BufNewFile */templates/*.y*ml,*/templates/*.tpl,*.gotmpl,helmfile*.yml set ft=helm
           autocmd BufRead,BufNewFile */templates/*.y*ml,*/templates/*.tpl,*.gotmpl,helmfile*.yml LspStop yammls
-          vim.opt.runtimepath = vim.opt.runtimepath + ${pkgs.vimPlugins.nvim-treesitter.withAllGrammars}/lua
+          vim.opt.runtimepath = vim.opt.runtimepath + ${pkgs.vimPlugins.nvim-treesitter.withAllGrammars}
+          require("start")
         ]])
         local configs = require('lspconfig.configs')
         local lspconfig = require('lspconfig')
