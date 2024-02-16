11d0cb54ca1c1706efac0a81a4c46d17bb5f0a11
Anthony Rabbito <hello@anthonyrabbito.com>
2024-02-15 19:16:20 -0500

    test

--- home-manager/personalities/cli/editor/nvim/default.nix
+++ home-manager/personalities/cli/editor/nvim/default.nix
@@ -21,7 +21,7 @@
         local configs = require('lspconfig.configs')
         local lspconfig = require('lspconfig')
         local util = require('lspconfig.util')
-        lvim.builtin.treesitter.compilers = { "gcc" }
+        require "nvim-treesitter.install".compilers = { "gcc" }
 
         if not configs.helm_ls then
           configs.helm_ls = {
