f13e2a60fa36d7bf1c3cba3c72d28bb8bc802263
Anthony Rabbito <hello@anthonyrabbito.com>
2024-04-02 21:38:02 -0400

    fix: home-manager nvd
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- home-manager/personalities/global/default.nix
+++ home-manager/personalities/global/default.nix
@@ -1,8 +1,9 @@
 { outputs, pkgs, lib, ... }: {
   imports = [ ../cli ] ++ (builtins.attrValues outputs.homeManagerModules);
   home.activation = {
-   reportChanges  = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      run ${pkgs.home-manager}/bin/home-manager generations | ${pkgs.coreutils}/bin/head -n 2 | ${pkgs.coreutils}/bin/bin/cut -d' ' -f 7 | ${pkgs.coreutils}/bin/tac | ${pkgs.findutils}/bin/xargs ${pkgs.nvd}/bin/nvd diff
+    diff = lib.hm.dag.entryBefore [ "installPackages" ] ''
+      [[ -z "''${oldGenPath:-}" ]] || [[ "$oldGenPath" = "$newGenPath" ]] || \
+         ${pkgs.nvd}/bin/nvd diff "$oldGenPath" "$newGenPath"
     '';
   };
 }
