2debc7031fcd0c47502e88f3179b9e814d82dee3
Anthony Rabbito <hello@anthonyrabbito.com>
2024-04-08 12:35:30 -0400

    fix: iterate all modules

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,7 +1,10 @@
-# Add your reusable home-manager modules to this directory, on their own file (https://nixos.wiki/wiki/Module).
-# These should be stuff you would like to share with others, not your personal configurations.
+let
+  modulesPerFile = {
+    fonts = import ./fonts.nix;
+  };
 
-{
-  # List your module files here
-  fonts = import ./fonts.nix;
-}
+  default = { ... }: {
+    imports = builtins.attrValues modulesPerFile;
+  };
+in
+modulesPerFile // { inherit default; }
