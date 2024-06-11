056a35b419b45602f77e0e98b1ec7455a77576db
Anthony Rabbito <hello@anthonyrabbito.com>
2024-06-11 13:54:33 -0400

    fix: syntax

--- home-manager/personalities/cli/direnv.nix
+++ home-manager/personalities/cli/direnv.nix
@@ -3,6 +3,6 @@
       enable = true;
       whitelist.prefix = [ "${config.home.homeDirectory}/dev" ];
       config.warn_timeout = 0;
-    }
+    };
     programs.direnv.nix-direnv.enable = true;
 }
