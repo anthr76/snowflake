e579cb130aad9ec35c13cdf6c747632940c51184
Anthony Rabbito <hello@anthonyrabbito.com>
2024-07-09 16:08:53 -0400

    fix: mount options
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- nixos/hosts/fw1-nwk2/disks.nix
+++ nixos/hosts/fw1-nwk2/disks.nix
@@ -18,6 +18,7 @@
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
+                mountOptions = [ "defaults" "umask=0077" ];
               };
             };
             root = {
