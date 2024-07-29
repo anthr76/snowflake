4fe69f7369de58bbb6914c5308f933506e201c7c
Anthony Rabbito <hello@anthonyrabbito.com>
2024-07-29 13:40:25 -0400

    feat: enable starship

--- home-manager/personalities/cli/starship.nix
+++ home-manager/personalities/cli/starship.nix
@@ -1,1 +1,14 @@
-{ programs.starship = { enable = true; }; }
+{ 
+    programs.starship = { 
+        enable = true;
+        kubernetes = {
+            disabled = false;
+            contexts = [
+                {
+                context_pattern = "^teleport\.[\w\.]+-(?P<cluster>[\w-]+)$";
+                context_alias = "$cluster";
+                }
+            ];
+        };
+    }; 
+}
