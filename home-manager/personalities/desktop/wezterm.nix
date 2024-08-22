2b78d45bbddfd8c7bb09853d8b60b7a47237a62a
Anthony Rabbito <hello@anthonyrabbito.com>
2024-08-21 20:52:11 -0400

    remove workaround

--- home-manager/personalities/desktop/wezterm.nix
+++ home-manager/personalities/desktop/wezterm.nix
@@ -8,7 +8,6 @@
         color_scheme = "Catppuccin Mocha",
         hide_tab_bar_if_only_one_tab = true,
         default_prog = { "${pkgs.fish}/bin/fish", "-l" },
-        front_end = "WebGpu",
       }
     '';
   };
