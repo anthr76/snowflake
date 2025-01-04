94bbf3756630facc73a1ae4fe4ee14bba6f99a10
Anthony Rabbito <hello@anthonyrabbito.com>
2025-01-04 11:06:55 -0500

    fix: disable fapi
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- nixos/personalities/physical/tpm2.nix
+++ nixos/personalities/physical/tpm2.nix
@@ -1,7 +1,10 @@
-{
+{ pkgs, ... }: {
   security.tpm2 = {
     enable = true;
     pkcs11.enable = true;
+    pkcs11.package = pkgs.tpm2-pkcs11.override {
+      fapiSupport = false;
+    };
     tctiEnvironment.enable = true;
     applyUdevRules = true;
   };
