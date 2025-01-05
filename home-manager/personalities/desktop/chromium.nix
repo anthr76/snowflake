f13a05a2fb842b4f7a37cbcf32e7c2e07a330ed8
Anthony Rabbito <hello@anthonyrabbito.com>
2025-01-05 17:46:16 -0500

    feat: add adnauseam
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- home-manager/personalities/desktop/chromium.nix
+++ home-manager/personalities/desktop/chromium.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }: {
+{ ... }: {
   home.sessionVariables = {
     GOOGLE_DEFAULT_CLIENT_ID = "77185425430.apps.googleusercontent.com";
     GOOGLE_DEFAULT_CLIENT_SECRET = "OTJgUOQcT7lO7GsGZq2G4IlT";
@@ -9,6 +9,16 @@
     extensions = [
       { id = "nngceckbapebfimnlniiiahkandclblb"; } # Bitwarden
       { id = "ckhlfagjncfmdieecfekjbpnnaekhlhd"; } # No Mouse Wheel Zoom
+      {
+        # adnauseam
+        id = "ilkggpgmkemaniponkfgnkonpajankkm";
+        crxPath = builtins.fetchurl {
+          name = "chromium-web-store.crx";
+          url = "https://github.com/dhowe/AdNauseam/releases/download/v3.24.0/adnauseam-3.24.0.chromium.crx";
+          sha256 = "sha256:0hy5x1q84q4alg412ic23fq39afn75kmfnmb475sd8bwvmr4v8q6";
+        };
+        version = "3.24.0";
+      }
     ];
     commandLineArgs = [
       "--ignore-gpu-blocklist"
