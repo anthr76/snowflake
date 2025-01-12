dd41ddf4f931e94b7cfb3be370a5db3bf88abbf5
Anthony Rabbito <hello@anthonyrabbito.com>
2025-01-12 13:52:52 -0500

    fix: enable widevine for chromium
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- home-manager/personalities/desktop/chromium.nix
+++ home-manager/personalities/desktop/chromium.nix
@@ -1,11 +1,13 @@
-{ ... }: {
+{ pkgs, ... }: {
   home.sessionVariables = {
     GOOGLE_DEFAULT_CLIENT_ID = "77185425430.apps.googleusercontent.com";
     GOOGLE_DEFAULT_CLIENT_SECRET = "OTJgUOQcT7lO7GsGZq2G4IlT";
   };
   programs.chromium = {
     enable = true;
-    # package = pkgs.google-chrome;
+    package = pkgs.chromium.override {
+      enableWideVine = true;
+    };
     extensions = [
       { id = "nngceckbapebfimnlniiiahkandclblb"; } # Bitwarden
       { id = "ckhlfagjncfmdieecfekjbpnnaekhlhd"; } # No Mouse Wheel Zoom
@@ -21,10 +23,11 @@
       }
     ];
     commandLineArgs = [
+      "--enable-features=AcceleratedVideoEncoder,VaapiOnNvidiaGPUs,VaapiIgnoreDriverChecks,Vulkan,DefaultANGLEVulkan,VulkanFromANGLE"
       "--ignore-gpu-blocklist"
       "--enable-logging=stderr"
       "--disable-features=WaylandFractionalScaleV1"
-      "--enable-features=TouchpadOverscrollHistoryNavigation"
+      "--enable-zero-copy"
       "--ignore-gpu-blocklist"
       # TODO: Work around Mesa issue @ d7ba0b445a025e6d105515527a78f1738e7e91be
       # https://github.com/NixOS/nixpkgs/issues/244742
