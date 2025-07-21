92967ca45ccca1df445240938143e002e9ada563
Anthony Rabbito <hello@anthonyrabbito.com>
2025-07-20 22:55:37 -0400

    fix: patch libavif
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,8 @@
 # This file defines overlays
-{ outputs, inputs }: {
+{
+  outputs,
+  inputs,
+}: {
   # For every flake input, aliases 'pkgs.inputs.${flake}' to
   # 'inputs.${flake}.packages.${pkgs.system}' or
   # 'inputs.${flake}.legacyPackages.${pkgs.system}'
@@ -23,7 +26,7 @@
   };
 
   # This one brings our custom packages from the 'pkgs' directory
-  additions = final: _prev: import ../pkgs { pkgs = final; };
+  additions = final: _prev: import ../pkgs {pkgs = final;};
 
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
@@ -40,9 +43,11 @@
     #   };
     # });
     gamescope = prev.gamescope.overrideAttrs (oldAttrs: {
-      patches = oldAttrs.patches ++ [
-        ./gamescope-native-res.patch
-      ];
+      patches =
+        oldAttrs.patches
+        ++ [
+          ./gamescope-native-res.patch
+        ];
     });
     linux-firmware-atkfix = prev.linux-firmware.overrideAttrs (oldAttrs: {
       # TODO: Fix when new firmware release is cut
@@ -57,28 +62,42 @@
     discord = prev.discord.overrideAttrs (oldAttrs: {
       withOpenASAR = true;
       withVencord = true;
-      postFixup = oldAttrs.postFixup or "" + ''
-        wrapProgram $out/bin/discord \
-        --add-flags "--ignore-gpu-blocklist " \
-        --add-flags "--disable-features=UseOzonePlatform " \
-        --add-flags "--enable-features=VaapiVideoDecoder " \
-        --add-flags "--use-gl=desktop " \
-        --add-flags "--enable-gpu-rasterization " \
-        --add-flags "--enable-zero-copy"
-      '';
+      postFixup =
+        oldAttrs.postFixup or ""
+        + ''
+          wrapProgram $out/bin/discord \
+          --add-flags "--ignore-gpu-blocklist " \
+          --add-flags "--disable-features=UseOzonePlatform " \
+          --add-flags "--enable-features=VaapiVideoDecoder " \
+          --add-flags "--use-gl=desktop " \
+          --add-flags "--enable-gpu-rasterization " \
+          --add-flags "--enable-zero-copy"
+        '';
     });
 
-    kdePackages = prev.kdePackages // {
-      sddm = prev.kdePackages.sddm.overrideAttrs (old: {
-        patches = (old.patches or []) ++ [
+    kdePackages =
+      prev.kdePackages
+      // {
+        sddm = prev.kdePackages.sddm.overrideAttrs (old: {
+          patches =
+            (old.patches or [])
+            ++ [
+              (final.fetchpatch {
+                url = "https://patch-diff.githubusercontent.com/raw/sddm/sddm/pull/1779.patch";
+                sha256 = "sha256-8QP9Y8V9s8xrc+MIUlB7iHVNHbntGkw0O/N510gQ+bE=";
+              })
+            ];
+        });
+      };
+    libavif = prev.libavif.overrideAttrs (oldAttrs: {
+      patches =
+        oldAttrs.patches or []
+        ++ [
           (final.fetchpatch {
-            url =
-              "https://patch-diff.githubusercontent.com/raw/sddm/sddm/pull/1779.patch";
-            sha256 = "sha256-8QP9Y8V9s8xrc+MIUlB7iHVNHbntGkw0O/N510gQ+bE=";
+            url = "https://patch-diff.githubusercontent.com/raw/AOMediaCodec/libavif/pull/2848.patch";
+            sha256 = "";
           })
         ];
-      });
-    };
+    });
   };
-
 }
