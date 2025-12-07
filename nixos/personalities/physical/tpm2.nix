f0124a754150078ef3e410f23971f27ef8a30a00
Anthony Rabbito <hello@anthonyrabbito.com>
2025-12-06 20:44:00 -0500

    fix: tpm2
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- nixos/personalities/physical/tpm2.nix
+++ nixos/personalities/physical/tpm2.nix
@@ -2,11 +2,8 @@
   security.tpm2 = {
     enable = true;
     pkcs11.enable = true;
-    pkcs11.package = pkgs.tpm2-pkcs11.override {
-      fapiSupport = false;
-    };
+    pkcs11.package = pkgs.tpm2-pkcs11-esapi;
     tctiEnvironment.enable = true;
     applyUdevRules = true;
   };
-  environment.sessionVariables.TSS2_LOG = "fapi+NONE";
 }
