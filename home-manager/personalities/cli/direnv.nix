da3166207e8922acb693f583361e05cd02951c31
Anthony Rabbito <arabbito@coreweave.com>
2026-04-27 12:12:28 -0400

    fix: stable direnv
    Signed-off-by: Anthony Rabbito <arabbito@coreweave.com>

--- home-manager/personalities/cli/direnv.nix
+++ home-manager/personalities/cli/direnv.nix
@@ -1,6 +1,7 @@
-{config, ...}: {
+{config, pkgs, ...}: {
   programs.direnv = {
     enable = true;
+    package = pkgs.stable.direnv;
     config.whitelist.prefix = ["${config.home.homeDirectory}/dev"];
     config.load_dotenv = true;
     config.warn_timeout = 0;
