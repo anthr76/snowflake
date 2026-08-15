1965000ed20a38b42490fbf1a0bc2fb00c7f3a0d
Anthony Rabbito <hello@anthonyrabbito.com>
2026-08-14 22:24:05 -0400

    fix: delta
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- home-manager/personalities/cli/git/git.nix
+++ home-manager/personalities/cli/git/git.nix
@@ -7,6 +7,7 @@ in {
   catppuccin.delta.enable = true;
   catppuccin.lazygit.enable = true;
   programs.delta = {
+    enable = true;
     enableGitIntegration = true;
   };
   # TODO: This needs to be modularized..
